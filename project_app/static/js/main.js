b9c4a5252f4b2e858d03e25872cfb6403c46997c
yjp24 <yjp24@DESKTOP-0I2FSCT>
2022-05-28 17:19:32 +0900

    자동완성

--- project_app/static/js/main.js
+++ project_app/static/js/main.js
@@ -1,6 +1,7 @@
 /**
  * 
  */
+ /*
 let items = false;
 	
 function update_selected() {
@@ -17,3 +18,15 @@ $(function() {
   $("#productSelect").change(update_selected);
   $("#productSelect").trigger("change");
 });
+*/
+$(function () {
+	$("#searchInput").autocomplete({
+		source: List,
+		focus : function(event, ui) {	
+			return false;
+		},
+		minLength: 1,
+		delay: 100,
+	});
+});
+List=['간장', '계란', '고추장', '과자', '기저귀', '껌', '냉동만두', '된장', '두루마리화장지', '두부', '라면', '마요네즈', '맛김', '맛살', '맥주', '밀가루', '분유', '사이다', '생리대', '생수', '샴푸', '설탕', '세탁세제', '소주', '시리얼', '식용류', '쌈장', '아이스크림', '어묵', '오렌지주스', '우유', '즉석밥', '참기름', '참치 캔', '커피', '케첩', '콜라', '햄']
