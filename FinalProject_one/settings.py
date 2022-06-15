a6cfb0e42f6a76d5df88bdd8b81902fc9d073187
kjh <wer2wer306@gmail.com>
2022-06-15 13:52:12 +0900

    rdb

--- FinalProject_one/settings.py
+++ FinalProject_one/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'FinalProject_one.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'testdb',
+        'USER': 'kjh',
+        'PASSWORD': 'testdb123',
+        'HOST': 'testdb.czax00bc0hzh.us-east-1.rds.amazonaws.com',
+        'PORT': 3306
+        
     }
 }
+
 
 
 # Password validation
