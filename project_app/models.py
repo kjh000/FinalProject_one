7faf32742aa35bbdd62f17352fb0a8c8aed97327
kjh <wer2wer306@gmail.com>
2022-06-15 23:48:28 +0900

    ㅇㅇ

--- project_app/models.py
+++ project_app/models.py
@@ -1,2 +1,26 @@
 from django.db import models
 
+class Basket(models.Model):
+    pname = models.CharField(max_length=50, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'basket'
+
+class Fbasket(models.Model):
+    pname = models.CharField(max_length=50, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'fbasket'
+
+
+class Gbasket(models.Model):
+    pname = models.CharField(max_length=50, blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'gbasket'
